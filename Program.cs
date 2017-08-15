857ed0c595bebf3bec9cef39e5acad1748cfb40f
Robby Bourne <robbybourne1@gmail.com>
2017-08-15 13:35:08 -0400

    All Called Functions are Workin

--- Program.cs
+++ Program.cs
@@ -4,9 +4,74 @@ namespace gamePiece_dotnet
 {
     class Program
     {
+        public class GamePiece
+        {
+            public int PositionX { get; set; } = 0;
+            public int PositionY { get; set; } = 0;
+            public bool Frozen { get; set; } = false;
+            public string Name { get; set; }
+            public string Color { get; set; }
+
+            public Boolean freeze()
+            {
+                Frozen = true;
+                return Frozen;
+            }
+
+            public Boolean unFreeze()
+            {
+                Frozen = false;
+                return Frozen;
+            }
+            public int MoveForward()
+            {
+             if (Frozen == false)
+             {
+                 PositionX++;
+                 PositionY++;
+                 return PositionX;
+             }
+             else
+             {
+                 return PositionX;
+             }
+            }
+            
+            public int MoveBack()
+            {
+                if (Frozen == false)
+                {
+                    PositionX--;
+                    PositionY--;
+                    return PositionX;
+                }
+                else
+                {
+                    return PositionX;
+                }
+            }
+            public override string ToString()
+            {
+                return $"{Name} {Color} has moved to X:{PositionX}, Y:{PositionY}.";
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var piece1 = new GamePiece();
+            piece1.Color = "Blue";
+            piece1.Name = "My Boy";
+
+            piece1.MoveForward();
+            Console.WriteLine(piece1);
+
+            piece1.freeze();
+            Console.WriteLine("You are frozen and Cant move");
+
+            piece1.unFreeze();
+            Console.WriteLine("You Have Been Unfrozen");
+
+            piece1.MoveBack();
+            Console.Write(piece1);
         }
     }
 }
